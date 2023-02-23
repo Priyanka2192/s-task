d1af3f4db820906d763cca1d7a1e8359374d23b2
Priyanka2192 <125649373+Priyanka2192@users.noreply.github.com>
2023-02-23 14:44:20 +0530

    Delete app.ts

--- ProjectTraining_1/packages/package1/app.ts
+++
@@ -1,9 +0,0 @@
-import express, { Application } from 'express';
-const app: Application = express();
-
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-
-    app.use('/product', require('./src/routes/getProduct'));
-
-export default app;
